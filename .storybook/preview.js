ec2b08779cae392a4147f71870a96bda0d08f134
Enrico Mrass <emrass@users.noreply.github.com>
2020-05-23 17:53:53 -0700

    Add A11y check to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,7 @@
 import '../src/index.css';
+
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+
+addDecorator(withA11y);
